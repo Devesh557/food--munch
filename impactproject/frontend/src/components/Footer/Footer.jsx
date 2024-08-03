341a5a1e99834cde4411b95fefb669b8567ade10
Devesh557 <131949030+Devesh557@users.noreply.github.com>
2024-08-03 08:41:32 +0530

    Footer.jsx

--- impactproject/frontend/src/components/Footer/Footer.jsx
+++ impactproject/frontend/src/components/Footer/Footer.jsx
@@ -24,6 +24,9 @@ const Footer = () => {
                     <li>foodmunch@gmail.com</li>
                 </ul>
             </div>
+            <div className='footer-content-center'>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1108.5895505550598!2d73.36493724139837!3d22.29105235079023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sparul%20university%20vadodara!5e0!3m2!1sen!2sin!4v1722653835703!5m2!1sen!2sin" width="400" height="300" ></iframe>
+            </div>
         </div>
       <hr />
       <p className='footer-copyright'>Copyright 2024 © Tomato.com All Right Reserved.k</p>
